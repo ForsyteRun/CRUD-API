9b0f4d49ca50bdb3541549227c12a862841afbb0
Dripa Alexandr <forsyteRun@gmail.com>
2024-02-12 09:34:08 +0200

    feat: add 3 test scenario

--- multi.ts
+++ multi.ts
@@ -3,29 +3,155 @@ import cluster from "node:cluster";
 import http from "node:http";
 import { availableParallelism } from "node:os";
 import process from "node:process";
+import * as url from "url";
+import { validate as isValidUUID, v4 as uuidv4 } from "uuid";
+// // import { getUsers, setUsers } from "./db";
+import { IUser } from "./types";
 
 dotenv.config();
+
+const initData: unknown[] = [];
 
 let port = process.env.PORT;
 const counter = port ? +port : 3000;
 
 if (cluster.isPrimary) {
-  for (let i = 0; i < availableParallelism(); i++) {
-    cluster.fork({ WORKER_PORT: counter + i });
+  for (let i = 0; i < availableParallelism() - 1; i++) {
+    const worker = cluster.fork({ WORKER_PORT: counter + i });
+
+    worker.on("message", function (msg) {
+      console.log("Worker to master: ", msg);
+
+      initData.push(msg.value?.data);
+
+      worker.send(initData);
+    });
   }
+
+  // Object.values(cluster.workers).forEach((worker) => {
+  //   worker.send({ message: "Hello from master!" });
+  // });
+
+  // cluster.on("message", (worker, message) => {
+  //   console.log(`Received message from worker ${message}:`, message);
+  //   if (message.type === "updateData") {
+  //     initData.push(message, 99);
+  //     console.log("Shared data updated:", initData);
+  //   } else if (message.type === "requestEnd") {
+  //     console.log(`Received end message from worker ${worker?.process.pid}`);
+  //     // Handle the end of request processing
+  //   }
+  // });
 
   cluster.on("exit", (worker) => {
     console.log(`Worker ${worker.process.pid} died`);
   });
 } else {
   const port = process.env.WORKER_PORT;
+  let data: Array<IUser | null> = [];
+  const childData: unknown[] = [];
 
-  const server = http.createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify({ msg: "success" }));
-  });
+  const server = http.createServer(
+    (req: http.IncomingMessage, res: http.ServerResponse) => {
+      // process.on("message", function (msg: Array<IUser | null>) {
+      //   try {
+      //     data = msg;
+      //   } catch (error) {
+      //     throw new Error();
+      //   }
+      //   // console.log("Master to worker:", msg, 77);
+      // });
+
+      // process.on("exit", function () {
+      //   console.log(data, "exit");
+      // });
+
+      try {
+        if (req.method === "GET" && req.url === "/users") {
+          // process.on("message", function (msg: Array<IUser | null>) {
+          //   try {
+          //     data = msg;
+          //   } catch (error) {
+          //     throw new Error();
+          //   }
+          //   // console.log("Master to worker:", msg, 77);
+          // });
+
+          // process.on("exit", function () {
+          //   console.log(data, "exit");
+          // });
+
+          process.on("message", function (msg: Array<IUser | null>) {
+            try {
+              data = msg;
+            } catch (error) {
+              throw new Error();
+            }
+
+            // const rmEmptyData = msg.filter((n) => n);
+            console.log("Master to worker:", data, 77);
+          });
+
+          res.writeHead(200, { "Content-Type": "application/json" });
+
+          res.end(JSON.stringify({ users: data }));
+        } else if (req.method === "POST" && req.url === "/users") {
+          let body = "";
+
+          req.on("data", (chunk) => {
+            try {
+              body += chunk.toString();
+            } catch (error) {
+              throw new Error();
+            }
+          });
+
+          req.on("end", () => {
+            try {
+              const { username, age, hobbies }: IUser = JSON.parse(body);
+
+              if (!username || !age || !hobbies) throw new Error();
+
+              const newUser: IUser = {
+                id: uuidv4(),
+                username: username,
+                age: age,
+                hobbies: hobbies,
+              };
+
+              cluster.worker?.send?.({
+                type: "updateData",
+                value: { data: newUser },
+              });
+
+              childData.push(newUser);
+              // process.send?.({ type: "requestEnd" });
+
+              res.end();
+            } catch {
+              throw new Error();
+            }
+          });
+        }
+      } catch {
+        res.writeHead(500, { "Content-Type": "application/json" });
+        res.end(JSON.stringify({ msg: "server side error" }));
+      }
+    }
+  );
 
   server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
+  });
+
+  process.on("message", function (msg: Array<IUser | null>) {
+    try {
+      data = msg;
+    } catch (error) {
+      throw new Error();
+    }
+
+    // const rmEmptyData = msg.filter((n) => n);
+    console.log("Master to worker:", childData, 77);
   });
 }
---
+++ src/api.test.ts
@@ -0,0 +1,197 @@
+import supertest from "supertest";
+import { server } from "./index";
+
+const userData = {
+  username: "JohnDoe",
+  age: 30,
+  hobbies: ["reading", "gaming"],
+};
+
+const modifyUser = {
+  username: "IvanDorn",
+  age: 110,
+  hobbies: ["reading"],
+};
+
+const request = supertest.agent(server);
+
+describe("scenario 1", function () {
+  // afterAll((done: jest.DoneCallback) => {
+  //   server.close(done);
+  // });
+
+  it("should get empty []", async () => {
+    const res = await request.get("/users");
+
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual([]);
+  });
+
+  it("should post user", async () => {
+    const res = await request.post("/users").send(userData);
+
+    expect(res.status).toBe(201);
+    expect(res.body).toEqual({ ...userData, id: expect.any(String) });
+  });
+
+  it("should GET user by id", async () => {
+    const userData = {
+      username: "JohnDoe",
+      age: 30,
+      hobbies: ["reading", "gaming"],
+    };
+
+    const postResponse = await request.post("/users").send(userData);
+    const { id } = postResponse.body;
+
+    const getResponse = await request.get(`/users/${id}`);
+
+    expect(getResponse.status).toBe(200);
+
+    expect(getResponse.body).toEqual(expect.objectContaining(userData));
+  });
+
+  it("should PUT user", async () => {
+    // const modifyUser = {
+    //   username: "IvanDorn",
+    //   age: 110,
+    //   hobbies: ["reading"],
+    // };
+
+    const postResponse = await request.post("/users").send(userData);
+    const { id } = postResponse.body;
+
+    const putResponse = await request.put(`/users/${id}`).send(modifyUser);
+
+    expect(putResponse.status).toBe(201);
+    expect(putResponse.body).toEqual({ ...modifyUser, id: expect.any(String) });
+  });
+
+  it("should DELETE user by id", async () => {
+    const postResponse = await request.post("/users").send(userData);
+    const { id } = postResponse.body;
+
+    const getResponse = await request.delete(`/users/${id}`);
+
+    expect(getResponse.status).toBe(204);
+  });
+
+  it("should GET removed user", async () => {
+    // const userData = {
+    //   username: "JohnDoe",
+    //   age: 30,
+    //   hobbies: ["reading", "gaming"],
+    // };
+
+    const postResponse = await request.post("/users").send(userData);
+    const { id } = postResponse.body;
+
+    await request.delete(`/users/${id}`);
+
+    const getResponse = await request.get(`/users/${id}`);
+
+    expect(getResponse.status).toBe(404);
+
+    expect(getResponse.body).toEqual({
+      msg: "404 Not Found",
+    });
+  });
+});
+
+describe("scenario 2", function () {
+  it("should get two users", async () => {
+    const res = await request.get("/users");
+
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual([
+      { ...userData, id: expect.any(String) },
+      { ...userData, id: expect.any(String) },
+      { ...modifyUser, id: expect.any(String) },
+    ]);
+  });
+
+  it("should POST & PUT user", async () => {
+    const modifyUser = {
+      username: "IvanDorn",
+      age: 110,
+      hobbies: ["reading"],
+    };
+
+    const postResponse = await request.post("/users").send(userData);
+    const { id } = postResponse.body;
+
+    const putResponse = await request.put(`/users/${id}`).send(modifyUser);
+
+    expect(putResponse.status).toBe(201);
+    expect(putResponse.body).toEqual({ ...modifyUser, id: expect.any(String) });
+  });
+
+  it("should GET user by id", async () => {
+    const userData = {
+      username: "JohnDoe",
+      age: 30,
+      hobbies: ["reading", "gaming"],
+    };
+
+    const postResponse = await request.post("/users").send(userData);
+    const { id } = postResponse.body;
+
+    const getResponse = await request.get(`/users/${id}`);
+
+    expect(getResponse.status).toBe(200);
+
+    expect(getResponse.body).toEqual(expect.objectContaining(userData));
+  });
+
+  it("should DELETE user by id", async () => {
+    const userData = {
+      username: "JohnDoe",
+      age: 30,
+      hobbies: ["reading", "gaming"],
+    };
+
+    const postResponse = await request.post("/users").send(userData);
+    const { id } = postResponse.body;
+
+    const getResponse = await request.delete(`/users/${id}`);
+
+    expect(getResponse.status).toBe(204);
+  });
+});
+
+describe("scenario 3", function () {
+  afterAll((done: jest.DoneCallback) => {
+    server.close(done);
+  });
+
+  it("should get two users", async () => {
+    const res = await request.get("/users");
+
+    expect(res.status).toBe(200);
+  });
+
+  it("should POST & PUT user", async () => {
+    const postResponse = await request.post("/users").send(userData);
+    const { id } = postResponse.body;
+
+    const putResponse = await request.put(`/users/${id}`).send(modifyUser);
+
+    expect(putResponse.status).toBe(201);
+    expect(putResponse.body).toEqual({ ...modifyUser, id: expect.any(String) });
+  });
+
+  it("should DELETE user by id", async () => {
+    const userData = {
+      username: "JohnDoe",
+      age: 30,
+      hobbies: ["reading", "gaming"],
+    };
+
+    const postResponse = await request.post("/users").send(userData);
+    const { id } = postResponse.body;
+
+    const getResponse = await request.delete(`/users/${id}`);
+
+    expect(getResponse.status).toBe(204);
+  });
+});
